b25d69609a85ed324524105b55f54836c1e73153
dbp <byronpantoja@gmail.com>
2021-12-30 06:40:06 +0800

    adding pauseButton

--- index.js
+++ index.js
@@ -4,21 +4,26 @@ class Timer {
     this.startButton = startButton;
     this.pauseButton = pauseButton;
 
-    this.startButton.addEventListener("click", this.start);
+    this.startButton.addEventListener('click', this.start);
+    this.pauseButton.addEventListener('click', this.pause);
   }
 
   start = () => {
     this.tick();
-    setInterval(this.tick, 1000);
+    this.interval = setInterval(this.tick, 1000);
+  };
+
+  pause = () => {
+    clearInterval(this.interval);
   };
 
   tick = () => {
-    console.log("tick");
+    console.log('tick');
   };
 }
 
-const durationInput = document.querySelector("#duration");
-const startButton = document.querySelector("#start");
-const pauseButton = document.querySelector("#pause");
+const durationInput = document.querySelector('#duration');
+const startButton = document.querySelector('#start');
+const pauseButton = document.querySelector('#pause');
 
 const timer = new Timer(durationInput, startButton, pauseButton);
